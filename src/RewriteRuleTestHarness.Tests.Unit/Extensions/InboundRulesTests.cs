e6d92b1694fad7810cf63297ad2fd1cd7561692b
Simon Colmer <simon_colmer@hotmail.com>
2016-12-16 14:42:30 +0000

    Added missing test

--- src/RewriteRuleTestHarness.Tests.Unit/Extensions/InboundRulesTests.cs
+++ src/RewriteRuleTestHarness.Tests.Unit/Extensions/InboundRulesTests.cs
@@ -28,5 +28,27 @@ namespace RewriteRuleTestHarness.Tests.Unit.Extensions
             // then
             Assert.That(rule, Is.EqualTo(rules.Rules[3]));
         }
+
+        [Test]
+        public void should_return_null_if_nothing_is_matched()
+        {
+            // given
+            var rules = new InboundRules
+            {
+                Rules = new[]
+                {
+                    new Rule { Match = new Match { Url = "^wrong-url$", IgnoreCase = true } },
+                    new Rule { Match = new Match { Url = "^another-wrong-url$", IgnoreCase = true } },
+                    new Rule { Match = new Match { Url = "^correct-URL$", IgnoreCase = false } },
+                    new Rule { Match = new Match { Url = "^correct-url$", IgnoreCase = true } },
+                }
+            };
+
+            // when
+            Rule rule = rules.FirstRuleToMatchUrl("NOPE");
+
+            // then
+            Assert.That(rule, Is.Null);
+        }
     }
 }
