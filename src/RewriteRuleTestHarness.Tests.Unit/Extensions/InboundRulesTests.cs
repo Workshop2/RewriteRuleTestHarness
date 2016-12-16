0ecbbf66f53c01c0035dba45015f6d4e43658b22
Simon Colmer <simon_colmer@hotmail.com>
2016-12-16 12:17:03 +0000

    Added extension to find first rule that matches url

---
+++ src/RewriteRuleTestHarness.Tests.Unit/Extensions/InboundRulesTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using RewriteRuleTestHarness.Models;
+using RewriteRuleTestHarness.Models.Extensions;
+
+namespace RewriteRuleTestHarness.Tests.Unit.Extensions
+{
+    [TestFixture]
+    public class InboundRulesTests
+    {
+        [Test]
+        public void should_return_expected_rule_when_url_matches()
+        {
+            // given
+            var rules = new InboundRules
+            {
+                Rules = new []
+                {
+                    new Rule { Match = new Match { Url = "^wrong-url$", IgnoreCase = true } },
+                    new Rule { Match = new Match { Url = "^another-wrong-url$", IgnoreCase = true } },
+                    new Rule { Match = new Match { Url = "^correct-URL$", IgnoreCase = false } },
+                    new Rule { Match = new Match { Url = "^correct-url$", IgnoreCase = true } },
+                }
+            };
+
+            // when
+            Rule rule = rules.FirstRuleToMatchUrl("correct-url");
+
+            // then
+            Assert.That(rule, Is.EqualTo(rules.Rules[3]));
+        }
+    }
+}
---
+++ src/RewriteRuleTestHarness/Models/Extensions/InboundRulesExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RewriteRuleTestHarness.Models.Extensions
+{
+    public static class InboundRulesExtensions
+    {
+        public static Rule FirstRuleToMatchUrl(this InboundRules inboundRules, string url)
+        {
+            foreach (Rule rule in inboundRules.Rules)
+            {
+                RegexOptions matchType = rule.Match.IgnoreCase ? RegexOptions.IgnoreCase : RegexOptions.None;
+                if (Regex.IsMatch(url, rule.Match.Url, matchType))
+                {
+                    return rule;
+                }
+            }
+
+            return null;
+        }
+    }
+}
